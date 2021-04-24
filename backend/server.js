af79eb073b043602c56af6d3b1476f880378ebac
Diego-Morardo <diegomorardo@gmail.com>
2021-04-23 22:29:32 -0300

    Levantando server con node

---
+++ backend/server.js
@@ -0,0 +1,18 @@
+import express from 'express';
+import data from './data.js'
+
+const app = express();
+
+app.get('/api/productos', (req, res) => {
+    res.send(data.productos)
+})
+
+app.get('/', (req, res) => {
+    res.send('Server is ready');
+});
+
+const puerto = process.env.PORT || 5000;
+
+app.listen(puerto, () => {
+    console.log(`Serve at http://localhost:${puerto}`);
+});
